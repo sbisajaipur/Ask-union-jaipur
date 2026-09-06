f54a159cc02b71bb516936b49d792a29823e0fa3
sbisajaipur <pareeksunil001@gmail.com>
2026-09-06 23:44:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,99 @@
-const CACHE_NAME = "ask-union-v7";
+const CACHE_NAME = "ask-union-v9";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
-  "./app.js",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+
+self.addEventListener("install", function(event) {
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+
+    caches.open(CACHE_NAME).then(function(cache) {
+
       return cache.addAll(FILES_TO_CACHE);
+
     })
+
   );
 
   self.skipWaiting();
+
 });
 
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", function(event) {
+
   event.waitUntil(
-    caches.keys().then(keys => {
+
+    caches.keys().then(function(keys) {
+
       return Promise.all(
+
         keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+          .filter(function(key) {
+
+            return key !== CACHE_NAME;
+
+          })
+
+          .map(function(key) {
+
+            return caches.delete(key);
+
+          })
+
       );
+
     })
+
   );
 
   self.clients.claim();
+
 });
 
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", function(event) {
+
+  /*
+    Firebase / Firestore requests should
+    always go directly to the network.
+  */
+
+  if (
+    event.request.url.includes("firestore.googleapis.com")
+  ) {
+
+    event.respondWith(
+      fetch(event.request)
+    );
+
+    return;
+
+  }
+
+
+  /*
+    app.js is no longer used by index.html.
+    Other requests use normal cache-first behaviour.
+  */
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+
+    caches.match(event.request).then(
+      function(response) {
+
+        return response || fetch(event.request);
+
+      }
+    )
+
   );
+
 });
