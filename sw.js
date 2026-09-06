0e80e76187a472cafe76660f7f2a2ee16a03ea62
sbisajaipur <pareeksunil001@gmail.com>
2026-09-06 23:25:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ask-union-v5";const CACHE_NAME = "ask-union-v3";
+const CACHE_NAME = "ask-union-v6";const CACHE_NAME = "ask-union-v5";const CACHE_NAME = "ask-union-v3";
 
 const FILES_TO_CACHE = [
   "./",
